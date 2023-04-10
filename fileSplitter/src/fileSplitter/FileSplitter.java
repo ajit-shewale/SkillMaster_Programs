1a7b7b0cd048bb2fb284fb8f89d3d4ff1abed601
ajitShewale <ajit.Shewale@prorigosoftware.com>
2023-04-10 16:44:31 +0530

    added validations

--- fileSplitter/src/fileSplitter/FileSplitter.java
+++ fileSplitter/src/fileSplitter/FileSplitter.java
@@ -6,16 +6,27 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileSplitter
 {
 
 	public static void main(String[] args){
-        
-		File file=new File("C:\\Users\\ajits\\OneDrive\\Desktop\\work\\cert_chain.pem");
-		if(file!=null && file.exists() == true) {
-			splitFile(file);
+
+		System.out.println("Please Provide path of input pem file :");
+		Scanner scanner = new Scanner(System.in);
+		String inputFile = scanner.nextLine();  		//eg.path - C:\\Users\\ajits\\OneDrive\\Desktop\\work\\cert_chain.pem
+		File file=new File(inputFile);
+		if(file!=null && file.exists() == true ) {
+			if(checkFileExtension(file.getName())) {
+				splitFile(file);
+			}else {
+				System.out.println("Input file is not PEM file. Please provide '.pem' files only.");
+			}
+		}else {
+			System.out.println("Given file does not Exists !!");
 		}
+		
 	}
 
 	private static void splitFile(File file) {
@@ -24,21 +35,21 @@ public class FileSplitter
 			BufferedReader br=new BufferedReader(fr);  
 			String line;
 			Boolean flag = false;
-			StringBuffer sb=new StringBuffer(); 
+			StringBuffer stringBuffer=new StringBuffer(); 
 			while((line=br.readLine())!=null) {
 					if(line.equalsIgnoreCase("-----END CERTIFICATE-----") && flag.equals(false)) {
-						sb.append(line); 
+						stringBuffer.append(line); 
 						//file create method.
-						generateFile(sb.toString(),"cert");
-						sb.setLength(0);
+						generateFile(stringBuffer.toString(),"cert");
+						stringBuffer.setLength(0);
 						flag=true;
 					}else {
-						sb.append(line);      
-						sb.append("\n"); 
+						stringBuffer.append(line);      
+						stringBuffer.append("\n"); 
 					}
 				}
 			if(flag = true) {
-				generateFile(sb.toString(),"chain");
+				generateFile(stringBuffer.toString(),"chain");
 			}
 			fr.close();     
 
@@ -47,22 +58,33 @@ public class FileSplitter
 		} 
 	}
 	
-	private static void generateFile(String sb , String fileName)
-	{
-		String path = "C:\\Users\\ajits\\OneDrive\\Desktop\\work\\certificates";
+	private static void generateFile(String dataString , String fileName){
+		String outputPath = "C:\\Users\\ajits\\OneDrive\\Desktop\\work\\certificates";
+		File outputfile = new File( outputPath.concat("\\").concat(fileName).concat(".pem"));
+		if(outputfile.exists() != true ) {
 		try {
-		      FileWriter myWriter = new FileWriter(path.concat("\\").concat(fileName).concat(".pem"));
-		      myWriter.write(sb.toString());
+		      FileWriter myWriter = new FileWriter(outputfile);
+		      myWriter.write(dataString);
 		      myWriter.close();
 		      System.out.println("Successfully wrote to the file.");
 		    } catch (IOException e) {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
-		
+		}else{
+			System.out.println("Output file Already exists!!");
+		}
 	}  
 	
-	
+	private static boolean checkFileExtension(String fileName){
+		int Index = fileName.lastIndexOf('.');
+		String extension = "";
+		if (Index > 0) {
+		    extension = fileName.substring(Index + 1);
+		}
+		
+		return extension.equals("pem")? true: false;
+	}  
 
 
 }
